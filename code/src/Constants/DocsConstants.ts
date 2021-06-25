89b42750e330200867fd9c4f3ab687da95540cf8
Sheepolution <danielhaazen@hotmail.com>
2021-06-25 19:07:50 +0200

    Change Lua's base url from 5.3 to 5.1

--- code/src/Constants/DocsConstants.ts
+++ code/src/Constants/DocsConstants.ts
@@ -1,6 +1,6 @@
 export default class SettingsConstants {
     public static readonly BASE_LOVE_DOCS_URL = 'https://love2d.org/wiki/';
-    public static readonly BASE_LUA_DOCS_URL = 'https://www.lua.org/manual/5.3/manual.html#pdf-';
+    public static readonly BASE_LUA_DOCS_URL = 'https://www.lua.org/manual/5.1/manual.html#pdf-';
 
     public static readonly API_NAMES = {
         LOVE: 'love',
